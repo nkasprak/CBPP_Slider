70cf032ca2d5ebd36218fe3eaa457a0ac6a0d2ca
nkasprak <nkasprak@gmail.com>
2016-11-15 11:41:27 -0500

    v0.5
    Ripped out a lot of the event code and started fresh. Wasn't working
    very well.

--- cbpp_slider.js
+++ cbpp_slider.js
@@ -15,9 +15,12 @@
         var jQueryUILoaded = false, jqueryCSSLoaded = false, sliderCSSLoaded = false,
             thisSliderLoaded = false;
         $.getScript(urlBase + "jquery-ui-1.11.4.custom/jquery-ui.min.js", function() {
-            jQueryUILoaded = true;
-            ready();
+            $.getScript(urlBase + "jquery.ui.touch-punch.min.js", function() {
+                ready();
+                jQueryUILoaded = true;
+            });
         });
+        
         function loadJQueryCSS() {
             jqueryCSSLoaded = true;
             ready();
@@ -74,7 +77,6 @@
             return false;
         }
         
-        
         var s = this;
 
         s.options = {
@@ -85,80 +87,13 @@
         
         s.selector = selector;
         $.extend(true,s.options,options);
-        s.changeHandlers = [];
-        s.change = function(changeHandler) {
-            if (typeof(changeHandler)==="undefined") {
-                s.doChangeHandlers();
-            } else {
-                s.changeHandlers.push(changeHandler);   
-            } 
-        };
-        s.doChangeHandlers = function() {
-            for (var i = 0,ii=s.changeHandlers.length;i<ii;i++) {
-                if (typeof(s.changeHandlers[i])==="function") {
-                    s.changeHandlers[i].call(s, [s.snapValue]);
-                }
-            }  
-        };
-        s.getValue = function() {
-            return s.snapValue;  
-        };
-        s.setValue = function(v) {
-            v = s.getSnapValue(v);
-            $(s.selector).slider("value",v);
-            s.snapValue = v;
-            s.sliderStopFunction(true);
-        };
-        s.getSnapValue = function(uiValue) {
-            var allowed = s.options.allowedValues,
-                fixedValue = uiValue;
-            if (typeof(allowed)!=="undefined"){
-                fixedValue = allowed[0];
-                for (var i = 0,ii=allowed.length;i<ii;i++) {
-                    if (uiValue >= allowed[i]) {
-                        if (i<ii-1) {
-                            fixedValue = Math.abs(uiValue - allowed[i]) <= Math.abs(uiValue - allowed[i+1]) ? allowed[i] : allowed[i+1];
-                        } else {
-                            fixedValue = allowed[i];
-                        }
-                    }        
-                }
-            }
-            return fixedValue;
-        };
-        s.sliderChangeFunction = function(ui, forceUpdate) {
-            if (typeof(forceUpdate)==="undefined") {
-                forceUpdate = false;
-            }
-            var timeoutFunction = function() {
-                var uiValue;
-                if (typeof(ui)!=="undefined") {
-                    uiValue = ui.value;
-                } else {
-                    uiValue = $(selector).slider("value");
-                }
-                var fixedValue = s.getSnapValue(uiValue);
-                if (fixedValue !== s.snapValue || forceUpdate) {
-                    s.snapValue = fixedValue;
-                    s.doChangeHandlers();
-                }
-            };
-            //clearTimeout(s.slideTimer);
-            timeoutFunction();
-            //s.slideTimer = setTimeout(timeoutFunction,0);
-        };
-        
-        s.sliderStopFunction = function(doChange) {
-            if (typeof(doChange) === "undefined") {
-                doChange = true;
-            }
-            if (doChange !== false) {
-                s.sliderChangeFunction();
-            }
-            if (typeof(s.snapValue) !== "undefined") {
-                $(selector).slider("value",s.snapValue);      
-            }
-        };
+
+        s.change = function(handler) {
+            $(s.selector).slider({
+                slide: handler
+            });
+        }
+
         s.makeTicks = function() {
             var toAppend = [], tick, perc;
             for (var i = 0,ii = s.options.labeledValues.length;i<ii;i++) {
@@ -183,37 +118,13 @@
         if (typeof(s.ready)==="function") {
             s.ready();
         }
-        s.options.slide = function(event, ui) {
-            s.sliderChangeFunction(ui);
-        };
-        s.options.stop = function() {
-            s.sliderStopFunction();
-        };
-        s.options.value = s.getSnapValue(s.options.value);
-        s.snapValue = s.options.value;
+        
         if (typeof(s.options.labeledValues)==="object") {
             $(s.selector).append(s.makeTicks());
         }
         $(s.selector).slider(s.options);
-        $(s.selector).on("touchstart touchmove", function(e) {
-            e.preventDefault();
-            var left = $(s.selector).offset().left,
-                width = $(s.selector).width(),
-                touch = e.originalEvent.touches[0],
-                percent,
-                value;
-            if (typeof(touch)!=="undefined") {
-                percent = (touch.pageX - left)/(width);
-                value = percent*(s.options.max - s.options.min) + s.options.min;
-                $(s.selector).slider("value",value);   
-            }
-            s.sliderChangeFunction();
-        });
-        $(s.selector).on("touchend", function() {
-            s.sliderStopFunction();
-        });
         $(s.selector).find(".ui-slider-handle").append("<div class='cbpp-slider-handle'></div>");
-        s.sliderStopFunction(false);
-        s.setValue(s.options.min);
+
+       
     };
 })();
